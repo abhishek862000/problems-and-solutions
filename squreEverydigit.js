c1424bacf60509e1430d61c51e571e858a4955d4
mrak862000 <mrak862000@gmail.com>
2024-10-06 00:08:17 +0530

    Add squreEverydigit.js to the project

---
+++ squreEverydigit.js
@@ -0,0 +1,11 @@
+function squareDigits(number){
+return (
+    number.toString()
+     .split('')
+     .map(ele => ele * ele)
+     .join('')
+)
+}
+console.log(squareDigits(2112)); // 4114
+console.log(squareDigits(3212)); // 9414
+console.log(squareDigits(9159)); // 8112581
